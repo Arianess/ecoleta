6c61c7f236f76defa8ca3fd2e88427af33c7242b
Ariane Silva <dev.arianess@gmail.com>
2020-06-07 03:17:15 -0300

    Aplicação Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,77 @@
+// coments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar= "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+//string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+// const s3 = `Isso é uma string`
+
+// document.write(s2)
+
+// number
+// const n1 = 1
+// const n2 = 12
+
+// document.write(n1 + n2)
+
+// boolean - true or false
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bTrue)
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetor
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionaliades - Atalhos - Reuso de código
+
+//registrar função
+// function sayMyName(name){
+
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyan")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if (notaFinal < 5) {
+//   // caminho 1
+//   document.write("Reprovado")
+// } else {
+//   // caminho 2
+//   document.write("Aprovado")
+// }
+
+
+//loop repeticoes
+
+// for (i=0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)  
+// }
